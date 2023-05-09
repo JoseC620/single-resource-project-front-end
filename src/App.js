2be6f044ce816e65bc0887e2d3e93bf9207de049
JoseC620 <Jcepeda6201@gmail.com>
2023-05-09 18:58:08 -0400

    linked backend and got all snacks showing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './Components/Nav';
+import Edit from './Pages/Edit';
+import Home from './Pages/Home';
+import Index from './Pages/Index';
+import New from './Pages/New';
+import Show from './Pages/Show';
 
 function App() {
-
-
-
 
 
 
   return (
     <div className='App'>
       <Router>
-        <NavBar />
+        <Nav />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
