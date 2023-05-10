1fdc4083084ba56d8f6d23a5e22394b2217de7d0
JoseC620 <Jcepeda6201@gmail.com>
2023-05-09 20:31:41 -0400

    index and details page working

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -2,19 +2,14 @@ import { Link } from 'react-router-dom';
 
 export default function Snack( { snack } ) {
 
-
-  
-
-
   return (
    <tr className='snack'>
     <td>
         <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
     </td>
-    <td>{snack}</td>  
-    <td>{snack}</td>
-    <td>{snack.is_healthy ? '♥️' : null}</td>
+    {/* <td>{snack.name}</td>   */}
+    <td> <img src={snack.image} alt='snack' width='100' height='100'/></td>
+    <td>{snack.is_healthy ? '❤️' : null}</td>
    </tr>
   );
 }
-// {snack.name}
