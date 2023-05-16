dafbde87f69d152e8c1ac6d8ec423e5d7817be54
JoseC620 <Jcepeda6201@gmail.com>
2023-05-15 20:32:15 -0400

    fixed errors

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -5,11 +5,11 @@ export default function Snack( { snack } ) {
 
   return (
    <Container>
-    <table>
-     <td>{snack.is_healthy ? '❤️' : null}</td>
+     <span>{snack.is_healthy ? '❤️' : null}</span>
      <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
-     <img class="rounded mx-auto d-block" src={snack.image} alt='snack' width='150' height='150'/>
-     </table>
+     <Link to={`/snacks/${snack.id}`}>
+     <img className="rounded mx-auto d-block" src={snack.image} alt='snack' width='150' height='150'/>
+     </Link>
    </Container>
   );
 }
