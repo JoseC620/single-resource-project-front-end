ad87103a14a41663e2b8cc3013fdc13d24aa6cb2
JoseC620 <Jcepeda6201@gmail.com>
2023-05-17 15:23:26 -0400

    netlify stuff

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,17 @@
-import NavBar from 'react-bootstrap/NavBar';
 import Container from 'react-bootstrap/Container';
+import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 
 export default function Nav() {
 
   return (
-    <NavBar bg='primary' expand='lg' varient='light'>
+    <Navbar bg='primary' expand='lg' varient='light'>
         <Container>
           <Link to={'/'} style={{color: "Black"}}>Home</Link>
             <Link to={'/snacks'} style={{color: "Black"}}>Snacks! click 🍪 here! </Link>
                 <Link to={'/snacks/new'} style={{color: "Black"}}>🍪 Make A New Snack!</Link>
         </Container>
-    </NavBar>
+    </Navbar>
   );
 }
